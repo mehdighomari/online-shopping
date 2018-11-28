092a4021a5bd3496af4ca7c01d44083df3b7c168
Mehdi Ghomari <mehdi.ghomari@outlook.fr>
2018-11-28 07:16:14 +0100

    try to github commit successful

---
+++ shoppingbackend/src/main/java/net/mehdi/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.mehdi.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
